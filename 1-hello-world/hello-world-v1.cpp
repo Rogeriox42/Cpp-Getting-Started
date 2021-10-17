de9c28a0f9c9c0ad54a7ab4fda5b3d6d19e31e97
RogerioXTI <sp.rogerio@outlook.com>
2021-10-17 20:21:44 -0300

    C++ Getting Started v1: Hello World

---
+++ 1-hello-world/hello-world-v1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std; 
+
+int main(){
+    cout << "Hello C++ World from VS Code" << endl; 
+    cout << "This is my 2nd line of text" << endl; 
+    
+    return 0; 
+}
---
+++ 1-hello-world/hello-world-v2.cpp
@@ -0,0 +1,20 @@
+#include<iostream> 
+
+using namespace std; 
+
+int main(){
+    cout << "Hello World!\n";
+    cout << "We're running C++!\n\n";
+    
+    cout << "It's a great start to understand how to output text to the user.\n";
+    cout << "We can use different ways to do it.\n\n";
+    
+    cout << "This is how we essentially share information with the user\n";
+    cout << "It can be many things, like: Menu Options, Form Input Information, \n";
+    
+    cout << "\nReceiving Data from the user, Outputting Data to the User, and many others.\n"; 
+    cout << "we'll approach each one of them in the next sessions :D" << endl;
+    
+    cout << "\nThanks & I hope you had a good time =] \nSee you in the variables session!";
+    return 0; 
+}
